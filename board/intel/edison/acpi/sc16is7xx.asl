nehalem-make/galileo-resurrection

board/intel: Take ASL samples from meta-acpi
History of updates (in reversed order):
  bd3e114e818d ("minnowboard-max: Fix leds.asl to follow binding schema")
  18ef1c9bb834 ("edison: Rename ds2-led -> arduino-leds-ds2 to be more particular")
  0e913eeae4c6 ("edison: Enable I²S muxing on Edison/Arduino")
  e6e25a0 ("minnowboard-max: Unify compatible string format")
  ebb8a76 ("edison: hx8347d: Add muxing matrix in the DefinitionBlock()")
  2709b50 ("edison: Correct data bit length field for ADS7951")
  e46c6cb ("edison: Add Bosch Sensortec BMP085 pressure sensor for Edison/Arduino")
  823a5db ("edison: ads1015: Couple vendor and product together")
  6c6b09d ("edison: Add support for SparkFun OLED block (DEV-13035)")
  7e684e0 ("acpi-tables: edison: Be consistent how to call _SB")
  497f677 ("acpi-tables: edison: Convert External() to use absolute path")
  c72d985 ("galileo: Revert "Don't use string literals for object references")

Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>

board/intel/edison/acpi/sc16is7xx.asl
@@ -1,0 +1,69 @@
+/*
+ * Intel Edison
+ *
+ * This adds NXP SC16IS7XX I2C-UART converter to the
+ * I2C#6  bus available on Intel Edison:
+ *
+ *   pin name           pin number
+ *   -----------------------------
+ *   I2C_6_SDA       112
+ *   I2C_6_SCL       111
+ *
+ * In Linux you need to set CONFIG_SERIAL_SC16IS7XX_CORE=y (or m) to be able to use
+ * this device.
+ *
+ * Copyright (C) 2018, Emlid Limited
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+DefinitionBlock ("serial.aml", "SSDT", 5, "", "SERIAL", 1)
+{
+    External (\_SB.PCI0.I2C6, DeviceObj)
+
+    Scope (\_SB.PCI0.I2C6)
+    {
+        Device (SER)
+        {
+            Name (_HID, "PRP0001")
+            Name (_DDN, "SC16IS7XX I2C-SERIAL")
+            Name (_CRS, ResourceTemplate () {
+                I2cSerialBus (
+                    0x4d,                   // Bus address
+                    ControllerInitiated,    // Don't care
+                    400000,                 // 400 kHz
+                    AddressingMode7Bit,     // 7-bit addressing
+                    "\\_SB.PCI0.I2C6",      // I2C host controller
+                    0                       // Must be 0
+                )
+
+                GpioInt(Level, ActiveLow, Exclusive, PullDefault, 0x0000,
+                        "\\_SB.PCI0.GPIO", 0x00, ResourceConsumer, , ) { 15 }
+
+            })
+
+            Name (_DSD, Package () {
+                ToUUID("daffd814-6eba-4d8c-8a91-bc9bbf4aa301"),
+                Package () {
+                    Package () { "compatible", Package () { "nxp,sc16is750" } },
+                    Package () { "clock-frequency", 14745600 },
+                }
+            })
+        }
+    }
+}

6b4tnt5vjs-spec/buildroot

  d272b0dbe532 ("edison: Add MAX6959 7-segment LED display controller for Edison")
  836b669aac89 ("edison: lsm9ds0: Use _HID in the outer device node")
  6a83ae434611 ("edison: Add Microchip MRF24J40 IEEE 802.15.4 2.4 GHz transceiver")
  a48a1c514978 ("edison: lsm9ds0: Add STMicro LSM9DS0 support")

board/intel/edison/acpi/sc16is7xx.asl
@@ -1,0 +1,69 @@
+/*
+ * Intel Edison
+ *
+ * This adds NXP SC16IS7XX I2C-UART converter to the
+ * I2C#6  bus available on Intel Edison:
+ *
+ *   pin name           pin number
+ *   -----------------------------
+ *   I2C_6_SDA       112
+ *   I2C_6_SCL       111
+ *
+ * In Linux you need to set CONFIG_SERIAL_SC16IS7XX_CORE=y (or m) to be able to use
+ * this device.
+ *
+ * Copyright (C) 2018, Emlid Limited
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+DefinitionBlock ("serial.aml", "SSDT", 5, "", "SERIAL", 1)
+{
+    External (\_SB.PCI0.I2C6, DeviceObj)
+
+    Scope (\_SB.PCI0.I2C6)
+    {
+        Device (SER)
+        {
+            Name (_HID, "PRP0001")
+            Name (_DDN, "SC16IS7XX I2C-SERIAL")
+            Name (_CRS, ResourceTemplate () {
+                I2cSerialBus (
+                    0x4d,                   // Bus address
+                    ControllerInitiated,    // Don't care
+                    400000,                 // 400 kHz
+                    AddressingMode7Bit,     // 7-bit addressing
+                    "\\_SB.PCI0.I2C6",      // I2C host controller
+                    0                       // Must be 0
+                )
+
+                GpioInt(Level, ActiveLow, Exclusive, PullUp, 0x0000,
+                        "\\_SB.PCI0.GPIO", 0x00, ResourceConsumer, , ) { 15 }
+
+            })
+
+            Name (_DSD, Package () {
+                ToUUID("daffd814-6eba-4d8c-8a91-bc9bbf4aa301"),
+                Package () {
+                    Package () { "compatible", Package () { "nxp,sc16is750" } },
+                    Package () { "clock-frequency", 14745600 },
+                }
+            })
+        }
+    }
+}